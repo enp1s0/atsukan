ea2a7a7d0a168031cab5d6e59ff1318816635fc8
mutsuki <mutsuki@momo86.net>
2018-12-08 12:37:26 +0900

    update : 表示モードを追加

--- src/kan.cu
+++ src/kan.cu
@@ -1,10 +1,10 @@
 #include "kan.hpp"
 
 template <class T>
-void kan::run(int num_sm, int num_cuda_core_per_sm, kan::algorithm_id algo){
+void kan::run(int num_sm, int num_cuda_core_per_sm, kan::algorithm_id algorithm_id, gpu_monitor::string_mode_id string_mode_id){
 
 }
 
-template void kan::run<float>(int, int, kan::algorithm_id);
-template void kan::run<double>(int, int, kan::algorithm_id);
+template void kan::run<float>(int, int, kan::algorithm_id, gpu_monitor::string_mode_id);
+template void kan::run<double>(int, int, kan::algorithm_id, gpu_monitor::string_mode_id);
 // instance
--- src/kan.hpp
+++ src/kan.hpp
@@ -1,12 +1,13 @@
 #ifndef __KAN_ALGORITHM_HPP__
 #define __KAN_ALGORITHM_HPP__
+#include "gpu_monitor.hpp"
 
 namespace kan{
 enum algorithm_id{
 	julia
 };
 template <class T>
-void run(const int num_sm, const int num_cuda_core_per_sm, const algorithm_id algo_id);
+void run(const int num_sm, const int num_cuda_core_per_sm, const algorithm_id algo_id, const gpu_monitor::string_mode_id string_mode);
 }
 
 #endif //__KAN_ALGORITHM_HPP__
