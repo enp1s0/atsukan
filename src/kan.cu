7d9096a3f24c35e69a43ca976ca697679fe44739
mutsuki <mutsuki@momo86.net>
2018-12-09 22:04:42 +0900

    update : juliaのmoduleを返すよう追記

--- src/kan.cu
+++ src/kan.cu
@@ -15,6 +15,8 @@ std::unique_ptr<kan_algorithm::kan_module<T>> get_kan_algorithm(const int gpu_id
 	switch (algorithm_id) {
 	case kan::algorithm_id::gemm:
 		ptr = new kan_algorithm::gemm<T>(gpu_id);
+	case kan::algorithm_id::julia:
+		ptr = new kan_algorithm::julia<T>(gpu_id, num_sm, num_cuda_core_per_sm);
 	default:
 		; // 世界で一番簡単な文
 	}
