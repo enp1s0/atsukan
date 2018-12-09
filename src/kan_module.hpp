d766a0a4d5f15aea474dc2c67f208b0dbbc5b371
mutsuki <mutsuki@momo86.net>
2018-12-09 21:04:34 +0900

    add kan_module.hpp

---
+++ src/kan_module.hpp
@@ -0,0 +1,18 @@
+#ifndef __KAN_MODULE_HPP__
+#define __KAN_MODULE_HPP__
+#include <vector>
+
+namespace kan_module{
+class module{
+	int gpu_id;
+	int num_sm;
+	int num_cuda_core_per_sm;
+public:
+	module(const int gpu_id, const int num_sm, const int num_cuda_core_per_sm);
+
+	// 燗アルゴリズムの実行
+	virtual void run(std::vector<int>& parameters) = 0;
+};
+} // kan_module
+
+#endif // __KAN_MODULE_HPP__
