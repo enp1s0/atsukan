1d31e6e3e60c135a6197caeee31ab5ef2100c3c3
mutsuki <mutsuki@momo86.net>
2018-12-12 16:19:04 +0900

    update : 例外が起きたら評価値を0として関数を抜ける

--- src/kan.cu
+++ src/kan.cu
@@ -31,49 +31,52 @@ std::unique_ptr<kan_algorithm::kan_base<T>> get_kan_algorithm(const int gpu_id, 
 
 template <class T>
 double kan::run(const int gpu_id, const int num_sm, const int num_cuda_core_per_sm, kan::algorithm_id algorithm_id, gpu_monitor::string_mode_id string_mode_id, const std::size_t computing_time, std::vector<int> run_arguments){
-	// start kan thread {{{
-	bool kan_complete = false;
-	auto kan_algorithm = get_kan_algorithm<T>(gpu_id, num_sm, num_cuda_core_per_sm, algorithm_id);
-	// 関数を抜けたら完了フラグを立てる
-	std::thread kan_thread([&kan_algorithm, &kan_complete, &run_arguments](){kan_algorithm.get()->run(kan_complete, run_arguments); std::cout<<"done"<<std::endl;});
-	// }}}
+	try{
+		// start kan thread {{{
+		bool kan_complete = false;
+		auto kan_algorithm = get_kan_algorithm<T>(gpu_id, num_sm, num_cuda_core_per_sm, algorithm_id);
+		std::thread kan_thread([&kan_algorithm, &kan_complete, &run_arguments](){kan_algorithm.get()->run(kan_complete, run_arguments); std::cout<<"done"<<std::endl;});
+		// }}}
 
-	// monitoring GPU {{{
-	gpu_monitor::monitor gpu_monitor(gpu_id);
-	const auto start_timestamp = std::time(nullptr);
-	if(string_mode_id != gpu_monitor::none){
-		if(string_mode_id == gpu_monitor::csv){
-			std::cerr<<"elapsed_time,";
-		}
-		std::cerr<<gpu_monitor.get_gpu_status_pre_string(string_mode_id)<<std::endl;
-	}
-	for(std::size_t time = 0; time < computing_time; time++){
-		const auto elapsed_time = std::time(nullptr) - start_timestamp;
-		gpu_monitor.get_gpu_status();
+		// monitoring GPU {{{
+		gpu_monitor::monitor monitor(gpu_id);
+		const auto start_timestamp = std::time(nullptr);
 		if(string_mode_id != gpu_monitor::none){
 			if(string_mode_id == gpu_monitor::csv){
-				std::cout<<elapsed_time<<",";
-			}else{
-				std::cout<<"["<<std::setw(6)<<elapsed_time<<"] ";
+				std::cerr<<"elapsed_time,";
 			}
-			std::cout<<gpu_monitor.get_gpu_status_string(string_mode_id)<<std::endl;
-			sleep(1);
+			std::cerr<<monitor.get_gpu_status_pre_string(string_mode_id)<<std::endl;
 		}
+		for(std::size_t time = 0; time < computing_time; time++){
+			const auto elapsed_time = std::time(nullptr) - start_timestamp;
+			monitor.get_gpu_status();
+			if(string_mode_id != gpu_monitor::none){
+				if(string_mode_id == gpu_monitor::csv){
+					std::cout<<elapsed_time<<",";
+				}else{
+					std::cout<<"["<<std::setw(6)<<elapsed_time<<"] ";
+				}
+				std::cout<<monitor.get_gpu_status_string(string_mode_id)<<std::endl;
+				sleep(1);
+			}
+		}
+		kan_complete = true;
+		// }}}
+		kan_thread.join();
+		const auto max_power = monitor.get_max_power()/1000.0;
+		if(string_mode_id != gpu_monitor::none){
+			const auto max_temperature = monitor.get_max_temperature();
+			std::cerr<<std::endl;
+			std::cerr<<"# Result"<<std::endl
+				<<"  - max temperature      : "<<max_temperature<<"C"<<std::endl
+				<<"  - max power            : "<<max_power<<"W"<<std::endl;
+		}
+		return max_power;
+	}catch(std::exception&){
+		return 0.0;
 	}
-	kan_complete = true;
-	// }}}
-	kan_thread.join();
 
-	const auto max_power = gpu_monitor.get_max_power()/1000.0;
-	if(string_mode_id != gpu_monitor::none){
-		const auto max_temperature = gpu_monitor.get_max_temperature();
-		std::cerr<<std::endl;
-		std::cerr<<"# Result"<<std::endl
-			<<"  - max temperature      : "<<max_temperature<<"C"<<std::endl
-			<<"  - max power            : "<<max_power<<"W"<<std::endl;
-	}
 
-	return max_power;
 }
 
 template <class T>
